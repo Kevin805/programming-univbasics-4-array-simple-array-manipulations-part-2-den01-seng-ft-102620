d6fe09c0dcd5e99fb15b8a654ce9199769a462ec
funny-module-5140 <kevin.michael805@gmail.com>
2020-10-15 19:26:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,8 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def using_insert(array,element)
-  array.insert
+def using_insert(array,new_element)
+  array.insert(new_element)
 end
 
 def using_uniq(array)
